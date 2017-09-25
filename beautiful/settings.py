9504f085ad51a00d06a03f9555aaa9b0c98b9a50
ofisser86 <ofisser86@gmail.com>
2017-09-25 15:24:57 +0800

    create app imbd

--- beautiful/settings.py
+++ beautiful/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from .db import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'imbd',
 ]
 
 MIDDLEWARE = [
@@ -73,12 +75,7 @@ WSGI_APPLICATION = 'beautiful.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+
 
 
 # Password validation
---
+++ imbd/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ImbdConfig(AppConfig):
+    name = 'imbd'
