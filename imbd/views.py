4fd327fc8a6d56ff12753e438465f9ded2fc37eb
ofisser86 <ofisser86@gmail.com>
2017-09-26 10:19:49 +0800

    add soup parser

--- imbd/views.py
+++ imbd/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from requests import get
+from bs4 import BeautifulSoup
 
 
 def imbdparser(request):
-    return render(request, 'imbd/index.html', {})
+    url = 'http://www.imdb.com/movies-in-theaters/'
+
+    response = get(url)
+
+    html_soup = BeautifulSoup(response.text, 'html.parser')
+    print(type(html_soup))
+
+    return render(request, 'imbd/index.html', {'response':response})
 
 # Create your views here.
