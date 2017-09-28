5c19c5786698fdbe3e70ce8d0d6140ffd7a682f1
ofisser86 <ofisser86@gmail.com>
2017-09-28 14:18:10 +0800

    correct view parser errors

--- imbd/views.py
+++ imbd/views.py
@@ -4,40 +4,44 @@ from bs4 import BeautifulSoup
 
 
 def imbdparser(request):
+    # url of current Week premiere
     url = 'http://www.imdb.com/movies-in-theaters/'
 
     response = get(url)
 
     html_soup = BeautifulSoup(response.text, 'html.parser')
+    # Get HTML block with film list and parse it
     movie_containers = html_soup.find_all('div', class_='list_item')
+    # Create list of  all movies for fill it later
     movie_list = []
-    # first_movie = movie_containers[0]
-    # stars_li = first_movie.select('span[itemprop="name"]')
-    # for i in stars_li:
-    #     print(type(i.text))
+    # ===========Test part============
+    #first_movie = movie_containers[0]
+    # stars_li = '|'.join(map(str, [stars.text for stars in first_movie.select('span[itemprop="name"]')]))
+    # print(stars_li)
+    # Get list of all films on parsed page
+    # f = first_movie.find('span', class_='metascore favorable')
+    # print(f)
+    # ========== End test ===========
     for movie in movie_containers:
+        # Check rating. Because Without check get an error NoneType object have not text attr
+        rating = movie.find('span', class_='metascore favorable')
+        if rating is not None:
+            rating = rating.text
+        else:
+            rating = '*'
         movie_list.append({
             'title':movie.h4.a['title'],
-            'description':movie.find('div', class_='outline').text,
+            'description':movie.find('div', class_='outline').string,
             'image': movie.img['src'],
-            'director': movie.h5.find_next('a').text
+            'director': movie.h5.find_next('a').text,
+            # Get stars list and clear from tags and \n, convert list to string
+            'stars': ','.join(map(str, [stars.text.strip('\n') for stars in movie.select('span[itemprop="actors"] > span')])),
+            # get rating
+            'rating': rating,
+            # Get rating list, clear from tags and \n, convert list to string
+            'genre': '|'.join(map(str, [genre.text.strip('\n') for genre in movie.select('span[itemprop="genre"]')])),
 
           }
         )
-        rating = movie.find('span', class_='metascore mixed')
-        if rating is not None:
-            movie_list.append({'rating':rating})
-        else:
-            movie_list.append({'rating': '*'})
-        genre_list = movie.select('span[itemprop="genre"]')
-        for genre in genre_list:
-            movie_list.append({'genre':genre.text})
-        # print(movie.select('span[itemprop="genre"]').text)
-        stars_list = movie.select('span[itemprop="name"]')
-        for star in stars_list:
-            movie_list.append({'star': star.text})
-
 
     return render(request, 'imbd/index.html', {'movie_list':movie_list})
-
-# Create your views here.
